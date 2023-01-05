a3cd077b89cf8473ddb1fc23c335f500a34c46c6
Max Larsson <maxslarsson@gmail.com>
2023-01-05 15:00:33 -0500

    Update base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://maxslarsson.github.io/interactive-graphics-portfolio"
+  base: "/interactive-graphics-portfolio/"
 })
